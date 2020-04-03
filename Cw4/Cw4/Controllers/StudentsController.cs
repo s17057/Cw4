f7ad7699bb5bf8bfe185c4d6715ea3398283069c
Dominik <dominikdomgalski@interia.pl>
2020-04-03 11:18:34 +0200

    Zadanie 4

---
+++ Cw4/Cw4/Controllers/StudentsController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cw4.Models;
+using Cw4.DAL;
+using System.Data.SqlClient;
+
+namespace Cw4.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+
+        private SqlConnection client;
+
+        public StudentsController()
+        {
+            client =  new SqlConnection("Data Source=db-mssql.pjwstk.edu.pl;Initial Catalog=s16870;Integrated Security=True");
+        }
+
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(string id)
+        {
+            IActionResult res;
+            var st = new Student();
+            var com = new SqlCommand();
+            com.Connection = client;
+            com.CommandText = "SELECT * FROM StudentAPBD s LEFT JOIN ENROLLMENT e ON s.IdEnrollment = e.IdEnrollment LEFT JOIN STUDIES st on e.IdStudy = st.IdStudy WHERE IndexNumber LIKE @id";
+            com.Parameters.AddWithValue("id", id);
+            client.Open();
+            var dr = com.ExecuteReader();
+            if (dr.HasRows) { 
+                while (dr.Read())
+                {
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    st.BirthDate = Convert.ToDateTime(dr["BirthDate"].ToString());
+                    st.StudyName = dr["Name"].ToString();
+                    st.Semester = Convert.ToInt32(dr["Semester"].ToString());
+
+                }
+                res = Ok(st);
+            }else
+            {
+                res = NotFound("Nie znaleziono studenta");
+            }
+            com.Dispose();
+            client.Dispose();
+            return res;
+        }
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var list = new List<Student>();
+            var com = new SqlCommand();
+            com.Connection = client;
+            com.CommandText = "SELECT * FROM StudentAPBD s LEFT JOIN ENROLLMENT e ON s.IdEnrollment = e.IdEnrollment LEFT JOIN STUDIES st on e.IdStudy = st.IdStudy";
+            client.Open();
+            var dr = com.ExecuteReader();
+            while (dr.Read())
+            {
+                var st = new Student();
+                st.FirstName = dr["FirstName"].ToString();
+                st.LastName = dr["LastName"].ToString();
+                st.IndexNumber = dr["IndexNumber"].ToString();
+                st.BirthDate = Convert.ToDateTime(dr["BirthDate"].ToString());
+                st.StudyName = dr["Name"].ToString();
+                st.Semester = Convert.ToInt32(dr["Semester"].ToString());
+
+                list.Add(st);
+            }
+            com.Dispose();
+            client.Dispose();
+            return Ok(list);
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student student)
+        {
+            return Ok($"Aktualizacja dokończona");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok($"Usuwanie ukończone");
+        }
+
+    }
+}
---
+++ Cw4/Cw4/DAL/IDbService.cs
@@ -0,0 +1,10 @@
+﻿using Cw4.Models;
+using System.Collections.Generic;
+
+namespace Cw4.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
